5613f9c7c533a9211919e90b865f5a0a2d52c018
tulsee <ramtulsi022@gmail.com>
2019-08-18 21:32:37 +0545

    implement the concept of local storage for the cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,9 @@ import {
     createStore,
     applyMiddleware
 } from "redux";
+import {
+    persistStore
+} from "redux-persist";
 import logger from 'redux-logger';
 
 import rootReducer from "./root-reducer";
@@ -9,7 +12,11 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 // const store = createStore(rootReducer, initalState, compose(applyMiddleware(...middlewares), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+export const persistor = persistStore(store);
 
-export default store;
+export default {
+    store,
+    persistor
+};
